ae00ed02da1c109251e1ff013bcd9073568a7eb8
Crowsing <47867877+Crowsing@users.noreply.github.com>
2021-06-22 17:43:08 +0300

    heroku deployment changes

--- config.py
+++ config.py
@@ -4,7 +4,7 @@ basedir = os.path.abspath(os.path.dirname(__file__))
 
 class Config(object):
     SECRET_KEY = os.environ.get('SECRET_KEY') or 'SecretKey01'
-    SQLALCHEMY_DATABASE_URI = 'postgres://mtclcpnvioxrdr:4eea05e39b462a4e20a42519db3f4bf45d6465b792b26ab9798e7699516ebaea@ec2-3-226-134-153.compute-1.amazonaws.com:5432/d8vfeai5knmsbn'
+    SQLALCHEMY_DATABASE_URI = 'postgresql://jyaewwljfdoeuu:0f37a43f1a66521aa5b8c5ebd80ff4f1832a2ccf13806beb5e7de372526de027@ec2-34-193-113-223.compute-1.amazonaws.com:5432/d3929hch22rdn6'
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     shop_id = 5
     secret_key = 'SecretKey01'
