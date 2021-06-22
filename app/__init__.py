f81bc9ce77902bab9635f823690f1e2dec94cfce
Crowsing <47867877+Crowsing@users.noreply.github.com>
2021-06-22 20:57:33 +0300

    add run server

--- app/__init__.py
+++ app/__init__.py
@@ -3,12 +3,13 @@ from config import Config
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
 
-if __name__ == '__main__':
-    app = Flask(__name__)
-    app.config.from_object(Config)
-    db = SQLAlchemy(app)
-    migrate = Migrate(app, db)
+app = Flask(__name__)
+app.config.from_object(Config)
+db = SQLAlchemy(app)
+migrate = Migrate(app, db)
 
+
+if __name__ == '__main__':
     app.run(debug=False)
 
 from app import routes, models
